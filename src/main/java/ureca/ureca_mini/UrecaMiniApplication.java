1a8142cc06da10b0e9ad6560ee4eec9f3f7594f9
marineAqu <wii021004@naver.com>
2025-05-14 14:32:58 +0900

    chore: DB 연결에 따른 exclude 제거

--- src/main/java/ureca/ureca_mini/UrecaMiniApplication.java
+++ src/main/java/ureca/ureca_mini/UrecaMiniApplication.java
@@ -2,10 +2,8 @@ package ureca.ureca_mini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//임시 어노테이션 (DB 연결 후 수정)
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class UrecaMiniApplication {
 
 	public static void main(String[] args) {
