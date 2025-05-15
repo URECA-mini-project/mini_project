c41388058c35984ab6393536e26401d82a2f0d6b
Doyeon Kim <wii021004@naver.com>
2025-05-15 16:19:41 +0900

    fix: DB 추가에 따른 exclude 삭제

--- src/main/java/ureca/ureca_mini/UrecaMiniApplication.java
+++ src/main/java/ureca/ureca_mini/UrecaMiniApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//임시 어노테이션 (DB 연결 후 수정)
 @SpringBootApplication
 public class UrecaMiniApplication {
 
